5ce4279802211c6d773a5725eeefc91dcb9ea22d
Shahrukh Khan <shahrukh@variablesoft.com>
2023-07-11 12:47:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationandnavigationdrawer/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.bottomnavigationandnavigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+     lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+    }
+}
